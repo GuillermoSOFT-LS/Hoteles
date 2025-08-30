d8f71e4b0e87b78e871270c92dafc23c3efd67c7
Guillermo1000 <102244432+Guillermo1000@users.noreply.github.com>
2025-08-30 13:13:17 -0400

    Se crearon las capas del proyecto, los modelos de las tablas y se edito el layout v0.3

---
+++ Entidad/Provincia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    internal class Provincia
+    {
+        public int IdProvincia { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
